162847e208b028046ae3abd4c426bdad9b2bae03
Kimon Vogt <kvogt@g.harvard.edu>
2023-12-03 16:01:47 -0500

    make the message explicit for checking connection with frontend

--- app/main.py
+++ app/main.py
@@ -43,7 +43,7 @@ app.include_router(auth.router)
 #send get request to our API #the path after the domain name of our API
 @app.get("/")  
 async def root():
-    return {"message": "welcome "}
+    return {"message": "welcome kimon  "}
 
 
 
